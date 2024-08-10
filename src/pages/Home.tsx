81318030379c655aa286136a7b7e5e9217a89398
Kudos <kudosbeluga@gmail.com>
2024-08-10 16:50:47 -0500

    routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,24 @@ const Home: React.FC = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+    let ticking = false;
 
+    const onScroll = () => {
+      if (!ticking) {
+        requestAnimationFrame(() => {
+          handleScroll();
+          ticking = false;
+        });
+        ticking = true;
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);   
   const backgroundStyle = {
     backgroundSize: `calc(100% + ${scrollY / 3}px) calc(100% + ${scrollY / 3}px)`,
     transition: 'background-size 0.1s'
@@ -23,7 +35,7 @@ const Home: React.FC = () => {
     <div className="home-section" style={backgroundStyle}>
       <div className="overlay"></div>
       <div className="content text-light">
-        <h1>Welcome to OrganiColors</h1>
+        <h1>Welcome to Organic  olors</h1>
         <p>Your source for beautiful framed artwork with a purpose.</p>
       </div>
     </div>
