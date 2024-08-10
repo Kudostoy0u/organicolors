2e6d7807c832bb2caf357bb5bb14c3852472dcd2
Kudos <kudosbeluga@gmail.com>
2024-08-10 13:34:39 -0500

    routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,10 @@
-// src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import './index.css';
-import Gallery from './pages/Gallery';
-import Navbar from './components/Navbar';
-import BottomBar from './components/BottomBar';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path="/" element={<App />}/>
-    <Route path="/gallery" element={<Gallery />} />
-    </>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Navbar/>
-    <RouterProvider router={router} />
-    <BottomBar/>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
